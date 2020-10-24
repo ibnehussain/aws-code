16501bddc3cc627e75541b4daf940d58e5994cd0
Azharbhaigit <63780759+Azharbhaigit@users.noreply.github.com>
2020-10-24 22:04:42 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "database-1.coghw13fheqo.us-east-2.rds.amazonaws.com"
-customuser = "root"
-custompass = "root1234"
-customdb = "employee2"
-custombucket = "addemp-1"
-customregion = "us-east-2"
